237de1a899bf660fd31d02ac2316fcbbc820e42f
Dina Sakæussen <disa5@student.eadania.dk>
2025-12-10 18:24:20 +0100

    testing commit - check github

---
+++ LaeringsportalenDaniaData/Main.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace LaeringsportalenDaniaData
+{ 
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //tester1234
+        //TesterGitHubRepo
+        //1234
+        //Kom
+    }
+}
---
+++ dinasOpgaveDaniadata/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace dinasOpgaveDaniadata
+{ 
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //kommentar
+        //sker der noget?
+        //Tredjeforsøg
+
+        // Test changes from Visual studio
+    }
+}
